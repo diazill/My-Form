dae01f369ab0d93761f2789f8413dce6da334a47
diazill <diazillyasa999@gmail.com>
2022-07-18 00:02:40 +0700

    Add delete module and function

---
+++ My Form/Pages/Home/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace My_Form.Pages.Home
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
